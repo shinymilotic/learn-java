b7e3c465df4a0d3b8b645a06519744c6fc631de5
shinymilotic <trungtin.mai1412@gmail.com>
2024-04-27 11:18:47 +0700

    MinimumSizeSubarraySum.java

---
+++ MinimumSizeSubarraySum.java
@@ -0,0 +1,35 @@
+public class MinimumSizeSubarraySum {
+
+    public int minSubArrayLen(int target, int[] nums) {
+        int maxSum = 0;
+        int left = 0;
+        int right = 0;
+        int minLen = nums.length;
+        while (right < nums.length) {
+            maxSum += nums[right];
+
+            while (maxSum >= target && left <= right) {
+                int afterPopedValue = maxSum - nums[left];
+                int distance = right - left + 1;
+                if (afterPopedValue < target && distance < minLen) {
+                    minLen = distance;
+                }
+                maxSum = afterPopedValue;
+                left++;
+            }
+
+            right++;
+        }
+
+        if (right == nums.length && left == 0) {
+            return 0;
+        }
+
+        return minLen;
+    }
+
+    public static void main(String[] args) {
+        MinimumSizeSubarraySum algo = new MinimumSizeSubarraySum();
+        algo.minSubArrayLen(7, new int[]{2,3,1,2,4,3}); 
+    }
+}
