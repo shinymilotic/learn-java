19dd4177db110cfa177ba0b55e5d990442635000
shinymilotic <trungtin.mai1412@gmail.com>
2024-04-29 15:58:57 +0700

    Treenode

---
+++ data_structure/TreeNode.java
@@ -0,0 +1,43 @@
+package data_structure;
+
+
+// Definition for a binary tree node.
+public class TreeNode {
+    private int val;
+    private TreeNode left;
+    private TreeNode right;
+    
+    TreeNode() {}
+
+    public TreeNode(int val) { this.val = val; }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void setLeft(TreeNode node) {
+        this.left = node;
+    }
+
+    public void setRight(TreeNode node) {
+        this.right = node;
+    }
+
+    public TreeNode getLeft() {
+        return this.left;
+    }
+
+    public TreeNode getRight() {
+        return this.right;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
